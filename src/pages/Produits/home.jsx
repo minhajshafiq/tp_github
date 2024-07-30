11defdbfddde94b06f74e6d5eeccb1397cbef043
minhajshafiq <minhaj93500@gmail.com>
2024-07-30 14:26:34 +0200

    Ajout : Page Produits & Détails Produits

---
+++ src/pages/Produits/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ListProduct from "../../components/ListProduct/listProduct";
+
+export default function Produits() {
+  return (
+    <main>
+      <ListProduct />
+    </main>
+  );
+}
